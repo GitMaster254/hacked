4a886b9dc7bf3d2a4d4594869fab12973ba6ca91
GitMater254 <161937061+GitMaster254@users.noreply.github.com>
2024-06-21 12:32:55 +0300

    Create hack.sh

---
+++ hack.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Function to display the hack notification
+display_hack_notification() {
+    # Display the notification
+    notify-send "System Alert" "You've been hacked!"
+    
+    # Add some suspense with a delay
+    sleep 3
+    
+    # Display a follow-up message
+    notify-send "Just Kidding!" "This is just a prank from your friend."
+}
+
+# Run the function
+display_hack_notification
